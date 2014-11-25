c316adb7c21df89a8091bbe1d7f912d5732c7fc2
Bernard Mordan <bernardmordan@gmail.com>
2014-11-25 10:10:18 +0000

    simple mongo fake id

--- pointer3.js
+++ pointer3.js
@@ -1,9 +1,24 @@
+Pointer = new Mongo.Collection('pointers')
+
 if (Meteor.isClient) {
+
+  var id = new Mongo.ObjectID
+
+  Template.phone.rendered = function(){
+    var el = this.find('section.phone')
+    Hammer(el).on('tap', function(e){
+      console.log(id._str)
+      Pointer.insert({_id: id._str,x: 0,y: 0})
+    })
+    Hammer(el).on('hammer.input', function(e){
+      Pointer.remove(id._str)
+    })
+  }
 
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+
   });
 }
